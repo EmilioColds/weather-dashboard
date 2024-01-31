ddcbac9571c02dc58b0988016ac565317e330a5b
EmilioColds <emiliofriastapia2705@outlook.com>
2024-01-31 12:48:35 -0600

    Added the var links with ID elements and API key

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var apiKey = '3118255833524eba6b72b13dacb447e1';
+    var searchButton = document.getElementById('search-button');
+    var cityInput = document.getElementById('city-input');
+    var currentWeather = document.getElementById('current-weather');
+    var forecast = document.getElementById('forecast');
+    var cityList = document.getElementById('city-list');
+});
